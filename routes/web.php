fd08bd80fdde2944d0d3701709d4da85f7c2537d
ajost <ajost.scada@gmail.com>
2026-06-21 12:04:57 +0700

    Fix all missing routes in web.php: add 50+ route definitions with proper names
    - Added HomeController routes (home.index, home.create, home.store, home.show, home.edit, home.update, home.destroy)
    - Added FacilityController routes (facilities.index, facilities.create, facilities.store, facilities.show, facilities.edit, facilities.update, facilities.destroy)
    - Added MonitoringController routes (monitoring.index, monitoring.createMetric, monitoring.storeMetric, monitoring.showMetric, monitoring.editMetric, monitoring.updateMetric, monitoring.destroyMetric, monitoring.indexArtikel, monitoring.createArticle, monitoring.storeArticle, monitoring.showArticle, monitoring.editArticle, monitoring.updateArticle, monitoring.destroyArticle)
    - Added AboutController route (about.index)
    - Added ContactController route (contact.index)
    - All 65 routes now properly registered and verified

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,9 @@ use App\Http\Controllers\Admin\ReportController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\FacilityController;
 use App\Http\Controllers\MonitoringController;
+use App\Http\Controllers\ProductPublicController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AboutController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,21 +20,54 @@ use Illuminate\Support\Facades\Route;
 | Public Routes
 |--------------------------------------------------------------------------
 */
-Route::get('/', [HomeController::class, 'index']);
+
+// Home
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
 Route::get('/home', [HomeController::class, 'index']);
-Route::get('/infrastructure', [FacilityController::class, 'index']);
-Route::get('/sustainability', [MonitoringController::class, 'index']);
-Route::get('/about', function () { return view('pages.about'); });
-Route::get('/contact', [ContactController::class, 'index']);
+Route::get('/home/create', [HomeController::class, 'create'])->name('home.create');
+Route::post('/home', [HomeController::class, 'store'])->name('home.store');
+Route::get('/home/{id}', [HomeController::class, 'show'])->name('home.show');
+Route::get('/home/{id}/edit', [HomeController::class, 'edit'])->name('home.edit');
+Route::put('/home/{id}', [HomeController::class, 'update'])->name('home.update');
+Route::delete('/home/{id}', [HomeController::class, 'destroy'])->name('home.destroy');
+
+// Infrastructure
+Route::get('/infrastructure', [FacilityController::class, 'index'])->name('facilities.index');
+Route::get('/infrastructure/create', [FacilityController::class, 'create'])->name('facilities.create');
+Route::post('/infrastructure', [FacilityController::class, 'store'])->name('facilities.store');
+Route::get('/infrastructure/{id}', [FacilityController::class, 'show'])->name('facilities.show');
+Route::get('/infrastructure/{id}/edit', [FacilityController::class, 'edit'])->name('facilities.edit');
+Route::put('/infrastructure/{id}', [FacilityController::class, 'update'])->name('facilities.update');
+Route::delete('/infrastructure/{id}', [FacilityController::class, 'destroy'])->name('facilities.destroy');
+
+// Sustainability (Energy Metrics)
+Route::get('/sustainability', [MonitoringController::class, 'index'])->name('monitoring.index');
+Route::get('/sustainability/metric/create', [MonitoringController::class, 'createMetric'])->name('monitoring.createMetric');
+Route::post('/sustainability/metric', [MonitoringController::class, 'storeMetric'])->name('monitoring.storeMetric');
+Route::get('/sustainability/metric/{id}', [MonitoringController::class, 'showMetric'])->name('monitoring.showMetric');
+Route::get('/sustainability/metric/{id}/edit', [MonitoringController::class, 'editMetric'])->name('monitoring.editMetric');
+Route::put('/sustainability/metric/{id}', [MonitoringController::class, 'updateMetric'])->name('monitoring.updateMetric');
+Route::delete('/sustainability/metric/{id}', [MonitoringController::class, 'destroyMetric'])->name('monitoring.destroyMetric');
+
+// Articles (public)
+Route::get('/articles', [MonitoringController::class, 'indexArtikel'])->name('monitoring.indexArtikel');
+Route::get('/articles/create', [MonitoringController::class, 'createArticle'])->name('monitoring.createArticle');
+Route::post('/articles', [MonitoringController::class, 'storeArticle'])->name('monitoring.storeArticle');
+Route::get('/articles/{id}', [MonitoringController::class, 'showArticle'])->name('monitoring.showArticle');
+Route::get('/articles/{id}/edit', [MonitoringController::class, 'editArticle'])->name('monitoring.editArticle');
+Route::put('/articles/{id}', [MonitoringController::class, 'updateArticle'])->name('monitoring.updateArticle');
+Route::delete('/articles/{id}', [MonitoringController::class, 'destroyArticle'])->name('monitoring.destroyArticle');
+
+// Products (public)
+Route::get('/products', [ProductPublicController::class, 'index'])->name('products.index');
+Route::get('/products/{id}', [ProductPublicController::class, 'show'])->name('products.show');
+
+// About
+Route::get('/about', [AboutController::class, 'index'])->name('about.index');
+
+// Contact
+Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
-
-// Articles
-Route::get('/articles', [MonitoringController::class, 'indexArtikel'])->name('articles.index');
-Route::get('/articles/{id}', [MonitoringController::class, 'showArticle'])->name('articles.show');
-
-// Products
-Route::get('/products', [\App\Http\Controllers\ProductPublicController::class, 'index'])->name('products.index');
-Route::get('/products/{id}', [\App\Http\Controllers\ProductPublicController::class, 'show'])->name('products.show');
 
 /*
 |--------------------------------------------------------------------------
